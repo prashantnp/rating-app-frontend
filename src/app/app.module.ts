ff41fb30c49842967837ef3cfb54e985f72ceec8
prashant.np <pnp1808@gmail.com>
2020-01-21 23:15:56 +0530

    Change imports order

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { NgxPaginationModule } from 'ngx-pagination';
 
 import { AppComponent } from './app.component';
 import { QuestionsComponent } from './questions/questions.component';
-import { HttpClientModule } from '@angular/common/http';
 import { NewQuestionComponent } from './questions/new-question/new-question.component';
 import { EditQuestionComponent } from './questions/edit-question/edit-question.component';
 
